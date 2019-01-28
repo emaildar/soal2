5fb008325cc4d93034ee0b55cd48c47d102c6b4e
emaildarlyn <emaildarlyn@gmail.com>
2019-01-28 15:18:33 +0700

    commit soal no.2

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res)=>{
     res.send('<h1>Tes Express & Mongoose</h1>')
 })
 
+app.use((req, res)=>{
+    res.status(404).send({"status":"404 Not Found"})
+})
+
 app.listen(1234, ()=>{
     console.log('server aktif di port 1234')
 })
